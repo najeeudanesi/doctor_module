0b7bdc5ed2e30df9d1c8f7a2120a315a8092722b
najiuOutsourceGlobal <najiu.danesi@outsourceglobal.net>
2024-03-18 14:52:36 -0700

    routes protected

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "./assets/css/general.css"
 import "./assets/css/index.css"
 import "./assets/css/pages-icons.css"
 import "./assets/css/pages-sidebar.css"
-import { createBrowserHistory } from 'history';
+
 import Homepage from './components/home';
-// import ProtectedRoute from './routes/ProtectedRoute';
 import { Toaster } from 'react-hot-toast';
-// import UserPortal from './components/UserPortal';
-// import Dashboard from './components/pages/Dashboard';
+
 import { MyProvider } from './contexts';
 import PageLayout from './components/layouts/PageLayout';
+
 
 
 
@@ -21,12 +20,13 @@ function App() {
 
   return (
     <MyProvider>
+      <div>
+        <BrowserRouter>
 
-      <Router >
-        <div>
           <Routes>
             <Route path="/" Component={Homepage} />
             <Route path="*" Component={PageLayout} />
+
             {/* <Route
               path="/doctor/*"
               element={
@@ -34,9 +34,11 @@ function App() {
             /> */}
             <Route render={() => <h1>Error 404. Page not found.</h1>} />
           </Routes>
-          <Toaster />
-        </div>
-      </Router>
+
+
+        </ BrowserRouter>
+        <Toaster />
+      </div>
     </MyProvider>
   );
 }
--- src/routes/PortalRoutes.js
+++ src/routes/PortalRoutes.js
@@ -1,21 +1,24 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import { Routes, Route } from 'react-router';
-
-
-// import ProtectedRoute from './ProtectedRoute';
 import Dashboard from '../components/pages/Dashboard';
 import Patients from '../components/pages/Patients';
 import Facility from '../components/pages/Facility';
 import CustomerManagement from '../components/pages/CustomerManagement';
 import PatientDetails from '../components/pages/PatientDetails';
+import AuthRoute from './AuthRoute';
+
+
+;
 
 export default () => (
+
     <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/patients" element={<Patients />} />
-        <Route path="/facility" element={<Facility />} />
-        <Route path="/customer-management" element={<CustomerManagement />} />
-        <Route path="/patient-details" element={<PatientDetails />} />
+        <Route path="/dashboard" element={<AuthRoute><Dashboard /></AuthRoute>} />
+        <Route path="/patients" element={<AuthRoute><Patients /> </AuthRoute>} />
+        <Route path="/facility" element={<AuthRoute><Facility /></AuthRoute>} />
+        <Route path="/customer-management" element={<AuthRoute><CustomerManagement /></AuthRoute>} />
+        <Route path="/patient-details" element={<AuthRoute><PatientDetails /></AuthRoute>} />
     </Routes>
+
 );
