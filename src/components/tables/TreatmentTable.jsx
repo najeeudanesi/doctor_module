5af0bc98f58359e3f92926b2d1367682356faa94
najiuOutsourceGlobal <najiu.danesi@outsourceglobal.net>
2024-04-24 15:20:58 +0100

    loading states

--- src/components/tables/TreatmentTable.jsx
+++ src/components/tables/TreatmentTable.jsx
@@ -3,8 +3,10 @@ import { get } from "../../utility/fetch";
 
 function TreatmentTable({ patientId }) {
     const [data, setData] = useState([]);
+    const [isloading, setIsLoading] = useState(false);
 
     const fetchData = async () => {
+        setIsLoading(true)
         try {
             const response = await get(`/patients/${patientId}/treatmentrecord`)
             console.log(response)
@@ -12,59 +14,63 @@ function TreatmentTable({ patientId }) {
         } catch (e) {
             console.log(e)
         }
-
+        setIsLoading(false)
     }
 
     useEffect(() => {
         fetchData()
     }, [])
     return (
-        <div className="w-100 ">
-
+        <div className="w-100">
             {
-                data ? (
-                    <div className="w-100 none-flex-item m-t-40">
-                        <table className="bordered-table-2">
-                            <thead className="border-top-none">
-                                <tr className="border-top-none">
-                                    <th>Date</th>
-                                    <th>Age</th>
-                                    <th>weight</th>
-                                    <th>Temp</th>
-                                    <th>Admin Nurse</th>
-                                    <th>Nurse Note</th>
-                                    <th>Diagnosis</th>
-                                    <th>Medication/Prescription</th>
+                !isloading ? (<div className="w-100 ">
 
-                                </tr>
-                            </thead>
+                    {
+                        data ? (
+                            <div className="w-100 none-flex-item m-t-40">
+                                <table className="bordered-table-2">
+                                    <thead className="border-top-none">
+                                        <tr className="border-top-none">
+                                            <th>Date</th>
+                                            <th>Age</th>
+                                            <th>weight</th>
+                                            <th>Temp</th>
+                                            <th>Admin Nurse</th>
+                                            <th>Nurse Note</th>
+                                            <th>Diagnosis</th>
+                                            <th>Medication/Prescription</th>
 
-                            <tbody className="white-bg view-det-pane">
-                                {data.map((row, index) => (
-                                    <tr key={index}>
-                                        <td>{new Date(row?.dateOfVisit).toLocaleDateString()}</td>
+                                        </tr>
+                                    </thead>
 
-                                        <td>{row?.age}</td>
-                                        <td>{row?.weight}kg</td>
-                                        <td>{row?.temperature}C</td>
-                                        <td>{row?.nurse}</td>
-                                        <td>{row?.additionalNote}</td>
-                                        <td>{row?.diagnosis}</td>
-                                        <td><ul>{row?.medications.map((medication, index) => (
-                                            <li key={index}>{medication.name}</li>
-                                        ))}</ul></td>
+                                    <tbody className="white-bg view-det-pane">
+                                        {data.map((row, index) => (
+                                            <tr key={index}>
+                                                <td>{new Date(row?.dateOfVisit).toLocaleDateString()}</td>
+
+                                                <td>{row?.age}</td>
+                                                <td>{row?.weight}kg</td>
+                                                <td>{row?.temperature}C</td>
+                                                <td>{row?.nurse}</td>
+                                                <td>{row?.additionalNote}</td>
+                                                <td>{row?.diagnosis}</td>
+                                                <td><ul>{row?.medications.map((medication, index) => (
+                                                    <li key={index}>{medication.name}</li>
+                                                ))}</ul></td>
 
 
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                ) : (
-                    <p>No data available</p>
-                )
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        ) : (
+                            <p>No data available</p>
+                        )
+                    }
+
+                </div>) : (<div>Loading....</div>)
             }
-
         </div>
     );
 }
